120c04d70ed2d98cb939765d3ec4255735a0e9e1
Luis cespedes <luis.cespedes@valposystems.com>
2024-04-22 10:43:07 -0400

    gitignore

--- src/main/java/com/lxdevelop/peliculas/controller/StreamController.java
+++ src/main/java/com/lxdevelop/peliculas/controller/StreamController.java
@@ -6,10 +6,15 @@ import com.lxdevelop.peliculas.service.VideoService;
 import com.lxdevelop.peliculas.service.VideoStreamingService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping("/api")
@@ -24,14 +29,23 @@ public class StreamController {
 
 
     @GetMapping("/stream/{videoId}")
-    public ResponseEntity<StreamingResponseBody> livestream(@PathVariable Long videoId,@RequestParam(required = false)  String codec) {
+    public ResponseEntity<StreamingResponseBody> livestream(@PathVariable Long videoId, @RequestParam(required = false) String codec) {
         Video video = videoService.findVideoById(videoId);
         if (video != null) {
             Codec codecEnum = Codec.fromString(codec);
             return ResponseEntity.ok()
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .body(outputStream -> videoStreamingService.streamVideo(video.getPath(), outputStream,codecEnum));
+                    .body(outputStream -> videoStreamingService.streamVideo(video.getPath(), outputStream, codecEnum));
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/stream2/{videoId}")
+    public ResponseEntity<UrlResource> getFullVideo(@PathVariable Long videoId) throws MalformedURLException {
+        Video video = videoService.findVideoById(videoId);
+        UrlResource videoResource = new UrlResource("file:" + video.getPath());
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory.getMediaType(videoResource).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .body(videoResource);
     }
 }
