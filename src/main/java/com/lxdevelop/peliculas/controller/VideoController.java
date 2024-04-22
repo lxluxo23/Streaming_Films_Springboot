1149d3e4deefb2ecaa9c3a3d09f1fd207092dbda
Luis cespedes <lxluxo23@gmail.com>
2024-04-22 15:03:44 -0400

    covers

--- src/main/java/com/lxdevelop/peliculas/controller/VideoController.java
+++ src/main/java/com/lxdevelop/peliculas/controller/VideoController.java
@@ -4,13 +4,12 @@ import com.lxdevelop.peliculas.model.Video;
 import com.lxdevelop.peliculas.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/api/videos")
 
 public class VideoController {
